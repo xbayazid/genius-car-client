09b998042fa9d994e8150f189064f71d807aca60
xbayazid <bayazid9006@gmail.com>
2022-11-03 21:52:42 +0600

    sign up page completed

---
+++ src/Pages/Signup/Signup.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import singUpImg from '../../assets/images/login/login.svg';
+
+const Signup = () => {
+    const handleSignUp = event =>{
+        event.preventDefault();
+    }
+    return (
+        <div className="hero w-full my-20">
+  <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
+    <div className="text-center lg:text-left">
+      <img className='w-3/4' src={singUpImg} alt="" />
+    </div>
+    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
+      <h1 className="text-5xl text-center font-bold">Sign Up</h1>
+      <form onSubmit={handleSignUp} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input type="text" placeholder="Full Name" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="text" placeholder="Email" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="text" placeholder="Password" className="input input-bordered" />
+        </div>
+        <div className="form-control mt-6">
+            <input className="btn btn-primary" type="submit" value="Sign Up" />
+        </div>
+      </form>
+      <p className='text-center'>Already have an account? <Link className='text-orange-600 font-bold' to='/login'>Login</Link></p>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default Signup;
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Home/Login/Login";
+import Signup from "../../Pages/Signup/Signup";
 
 const router = createBrowserRouter([
     {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
         {
           path: '/login',
           element: <Login></Login>
+        },
+        {
+          path: '/signup',
+          element: <Signup></Signup>
         }
       ]
     }
