6bbbb09ec87a3981fc8045e5a9e2bdd0c6cb0dd0
xbayazid <bayazid9006@gmail.com>
2022-11-03 21:41:33 +0600

    login page created

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -6,6 +6,7 @@ const Header = () => {
 
     const menuItem = <>
     <li className='font-semibold'><Link to='/'>Home</Link></li>
+    <li className='font-semibold'><Link to='/login'>Login</Link></li>
     </>
     return (
         <div className="navbar h-20 mb-12 pt-12 bg-base-100">
