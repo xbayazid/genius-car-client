d6c34102349fb70af15f6cb5840066464e147944
xbayazid <bayazid9006@gmail.com>
2022-11-03 14:42:17 +0600

    carouselr completed

---
+++ src/Pages/Home/Banner/BannerItem.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const BannerItem = ({slide}) => {
+    const {image, id, prev, next} = slide;
+    return (
+        <div id={`slide${id}`} className="carousel-item relative w-full">
+        <div className='carousel-img'>
+        <img src={image} alt="" className="w-full" />
+        </div>
+         <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4">
+           <h1 className='text-6xl font-bold text-white'>
+             Affordable <br />
+             Price for Car <br />
+             Serviceing
+           </h1>
+         </div>
+         <div className="absolute w-2/5 flex justify-end transform -translate-y-1/2 left-24 top-1/2">
+           <p className='text-xl text-white'>
+           There are many variations of passages of  available, but the majority have suffered alteration in some form
+           </p>
+         </div>
+         <div className="absolute w-2/5 flex justify-start transform -translate-y-1/2 left-24 top-3/4">
+         <button className="btn btn-secondary mr-5">Discover More</button>
+         <button className="btn btn-outline btn-warning">Latest Project</button>
+         </div>
+         <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+           <a href={`#slide${prev}`} className="btn btn-circle mr-5">❮</a> 
+           <a href={`#slide${next}`} className="btn btn-circle">❯</a>
+         </div>
+       </div> 
+    );
+};
+
+export default BannerItem;
